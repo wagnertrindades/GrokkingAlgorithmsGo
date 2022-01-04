a01c6acd911c67e0dcb73dce60ce7c65d627aa51
wtrindades <wtrindades@gmail.com>
2022-01-03 21:30:54 -0300

    Add binary search

---
+++ binary_search/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	my_list := [6]int{1, 3, 5, 7, 9, 10}
+
+	fmt.Println(binary_search(my_list[:], 3))
+	fmt.Println(binary_search(my_list[:], -1))
+	fmt.Println(binary_search(my_list[:], 7))
+}
+
+func binary_search(list []int, item int) (int, bool) {
+	low := 0
+	high := len(list) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		try := list[mid]
+
+		if try == item {
+			return mid, true
+		}
+
+		if try > item {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return 0, false
+}
